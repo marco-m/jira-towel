3e032fdceaf6a590ec8ea3936505ed1bd129c6af
Marco Molteni <marco.molteni@mailbox.org>
2024-09-19 22:32:38 +0200

    add goreleaser

--- internal/version.go
+++ internal/version.go
@@ -5,8 +5,11 @@ import (
 	"runtime/debug"
 )
 
-// linkerVersion must be set by the linker (see Taskfile).
-var linkerVersion = "broken build!"
+// These variables must be set by the linker (see Taskfile or .goreleaser.yaml).
+var (
+	version = "unknown"
+	commit  = "unknown"
+)
 
 // Version reports the version of the main package of the binary.
 //
@@ -15,12 +18,13 @@ var linkerVersion = "broken build!"
 // 1. to support "go build", we must use the linker.
 // 2. to support "go install with remote path", we must use the `debug` package.
 // 3. as far as I understand it, "go install with local path" does not work.
-// See https://github.com/golang/go/issues/50603, to be able to use the  `debug`
+// See https://github.com/golang/go/issues/50603, to be able to use the `debug`
 // package for all use cases.
+// Wow. It seems that finally Go 1.24 will do the right thing.
 func Version() string {
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
-		return fmt.Sprintf("%s (stripped)", linkerVersion)
+		return fmt.Sprintf("%s (stripped)", version)
 	}
 	mod := &info.Main
 	if mod.Replace != nil {
@@ -29,5 +33,5 @@ func Version() string {
 	if mod.Version != "" && mod.Version != "(devel)" {
 		return fmt.Sprintf("%s (%s)", mod.Version, mod.Path)
 	}
-	return fmt.Sprintf("%s (%s)", linkerVersion, mod.Path)
+	return fmt.Sprintf("%s (%s)", version, mod.Path)
 }
